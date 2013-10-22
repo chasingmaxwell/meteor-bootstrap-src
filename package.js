b4c8b603a440aeb3b6940a9c1cd10a78ac28bf67
Peter Sieg <chasingmaxwell@gmail.com>
2013-10-21 21:25:43 -0400

    initial commit

---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  summary: "Twitter Bootstrap source files."
+});
+
+Package.on_use(function (api) {
+  api.use(['less'], 'client');
+
+  api.add_files([
+    'less/bootstrap.less',
+    'less/responsive.less',
+    'js/bootstrap-affix.js',
+    'js/bootstrap-alert.js',
+    'js/bootstrap-button.js',
+    'js/bootstrap-carousel.js',
+    'js/bootstrap-collapse.js',
+    'js/bootstrap-dropdown.js',
+    'js/bootstrap-modal.js',
+    'js/bootstrap-tooltip.js',
+    'js/bootstrap-popover.js',
+    'js/bootstrap-scrollspy.js',
+    'js/bootstrap-tab.js',
+    'js/bootstrap-transition.js',
+  ], 'client');
+});
+
